1eb8621acf067e3a28309a1af63dd11a135202e1
Luis Felipe <felipenarciso15@gmail.com>
2025-01-31 00:29:58 -0300

    base url da api configurada com sucesso

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,18 @@
-// import axios from "axios";
+import axios from "axios";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const api = axios.create({
+  baseURL: process.env.BASE_URL,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API error:", error.response || error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
